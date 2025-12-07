e075d224a5f1edb5fac9a2a3b77230823b0f9d33
elwhyjay <jqueen.astro@gmail.com>
2025-12-07 03:06:52 +0000

    update swiglu kernel better perform

--- src/swiglu.cu
+++ src/swiglu.cu
@@ -1,5 +1,18 @@
 #include <cuda_runtime.h>
 
+
+__global__ void swiglu_big(const float* __restrict__ input, float* __restrict__ output, int N) {
+    int tid = threadIdx.x + (blockDim.x * blockIdx.x);
+    float inp, inp2;
+    for(int i = tid; i < N; i += (gridDim.x * blockDim.x)) {
+        inp = input[i];
+        inp2 = input[i + N];
+        inp = inp / (1.0f + __expf(-inp));
+        inp *= inp2;
+        output[i] = inp;
+    }
+}
+
 __global__ void swiglu_kernel(const float* input, float* output, int halfN) {
     int tid = threadIdx.x + (blockDim.x * blockIdx.x);
     float tmp1=0.0f, tmp2 = 0.0f;
